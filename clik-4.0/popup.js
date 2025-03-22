f217f39d63d6f54e7b92d867ac241b06f3987afb
ivan-os07 <138881010+ivan-os07@users.noreply.github.com>
2025-03-22 23:29:32 +0500

    Delete clik-4.0 directory

--- clik-4.0/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('myButton').addEventListener('click', () => {
-	// Отправляем сообщение в content script
-	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-		chrome.tabs.sendMessage(tabs[0].id, { action: 'runRokiClick' })
-	})
-    window.close();
-})
